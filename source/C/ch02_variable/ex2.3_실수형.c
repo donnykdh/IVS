58849c4a6ac156af3d789a6426766f243555524f
donnykdh <donny0331@naver.com>
2024-05-28 08:53:55 +0900

    second

---
+++ source/C/ch02_variable/ex2.3_실수형.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// 실수형 (float 4byte, double 8byte)
+// 정수형 (int 4byte, long long 8byte)
+//int main(void) {
+//	int age = 20;
+//	long long money = 2200000000L;
+//	float f = 3.14195265359F;
+//	double d = 3.14195265359;
+//	printf("f=%.11f\n", f);
+//	printf("d=%.11lf\n", d);
+//
+//	f = 10.1;
+//	d = 10.1;
+//	printf("f=%.11f\n", f);
+//	printf("d=%.11lf\n", d);
+//}
---
+++ source/C/ch02_variable/ex6_프로젝트.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	char name[100], gender[3];
+	int korean, english, math;
+	printf("이름을 입력하세요 : ");
+	scanf_s("%s", name, (unsigned)sizeof(name));
+	printf("성별을 입력하세요 (남/여) : ");
+	scanf_s("%s", gender, (unsigned)sizeof(gender));
+	printf("국어 영어 수학 점수를 입력하세요 : ");
+	scanf_s("%d %d %d", &korean, &english, &math);
+
+	printf("---학생정보---\n");
+	printf("이름 : %s\n", name);
+	printf("성별 : %s\n", gender);
+	printf("국어 = %d\n", korean);
+	printf("영어 = %d\n", english);
+	printf("수학 = %d\n", math);
+	printf("합계 = %d\n", korean + english + math);
+	printf("평균 = %.2lf\n", (korean+english+math)/3.0);
+}
+
